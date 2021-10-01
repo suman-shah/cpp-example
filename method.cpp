672ccc2cb8202199a0ca09cac0aebf5482f36191
MoneyPlantyoy <91759053+MoneyPlantyoy@users.noreply.github.com>
2021-10-02 01:57:47 +0545

    methods

---
+++ method.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+  public:
+    int speed(int maxSpeed);
+};
+
+int Car::speed(int maxSpeed) {
+  return maxSpeed;
+}
+
+int main() {
+  Car myObj; // Create an object of Car
+  cout << myObj.speed(200); // Call the method with an argument
+  return 0;
+}
