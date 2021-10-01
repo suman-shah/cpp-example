05fa702aa0acbd74b244051e6a9115da391e84ed
MetallicHeart <78961059+MetallicHeart@users.noreply.github.com>
2021-10-01 12:32:01 +0545

    Boolean in cpp

---
+++ math.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  bool isCodingFun = true;
+  bool isFishTasty = false;
+  cout << isCodingFun << "\n";
+  cout << isFishTasty;
+  return 0;
+}
