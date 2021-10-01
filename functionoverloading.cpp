80a74acd888d51ed0273f61010992a7bfa8fb76b
Kamete65 <91612300+Kamete65@users.noreply.github.com>
2021-10-01 13:48:59 +0545

    function overloading in cpp

---
+++ functionoverloading.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int plusFuncInt(int x, int y) {
+  return x + y;
+}
+
+double plusFuncDouble(double x, double y) {
+  return x + y;
+}
+
+int main() {
+  int myNum1 = plusFuncInt(8, 5);
+  double myNum2 = plusFuncDouble(4.3, 6.26);
+  cout << "Int: " << myNum1 << "\n";
+  cout << "Double: " << myNum2;
+  return 0;
+}
