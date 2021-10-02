f7b996ecdc4185a55b053c98eb58334da3f92aae
sejx <91691666+sejx@users.noreply.github.com>
2021-10-03 00:25:03 +0530

    Update string.cpp

--- strings.cpp
+++ strings.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-  string greeting = "Hello";
-  cout << greeting;
-  return 0;
-}
+#include <iostream>  
+using namespace std;  
+int main( ) {  
+    string s1 = "Hello";    
+        char ch[] = { 'C', '+', '+'};    
+        string s2 = string(ch);    
+        cout<<s1<<endl;    
+        cout<<s2<<endl;    
+}  
