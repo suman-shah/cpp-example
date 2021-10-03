3fa893d7bde5594f41aa0fcf8c19ff4131594528
samyak260702 <77425209+samyak260702@users.noreply.github.com>
2021-10-03 10:23:13 +0530

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  int array[n];
+ for(int i=0;i<n;i++){
+ cin>>arr[i]; 
+ }
+   for(int i=0;i<n;i++){
+ cout>>arr[i]; 
+ }
+  
+  return 0;
+}
